b64943d0face40be1bf0679a891757342e8e2a60
florianrabbah <floflo97231@live.fr>
2023-11-07 19:52:39 +0100

    yessir

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,10 +1,44 @@
 class AttendancesController < ApplicationController
+  
+  def index
+  
+    @attendances = Attendance.all
+  end
   def new
+    @attendance = Attendance.new
+
   end
 
   def create
+    @attendance = Attendance.new(attendance_params)
+    @event = Event.find(params[:event_id])
+    @attendance.event = @event
+    @attendance.user = current_user
+  
+    if @attendance.save
+      redirect_to @attendance, notice: 'Attendance was successfully created.'
+    else
+      render :new
+    end
   end
 
+  def edit
+    @attendance = Attendance.find(params[:id])
+  end
+  def show
+    @attendance = Attendance.find(params[:id])
+  end
   def destroy
   end
+
+private
+
+def attendance_params
+  params.require(:attendance).permit(:event_id) # Ajoutez d'autres attributs au besoin
+end
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "welcome#index"
+  resources :attendances
+
 end
