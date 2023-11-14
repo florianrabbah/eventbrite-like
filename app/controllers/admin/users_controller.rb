5d1b5c6563140a041357a802867b9030fc4f9e6f
florianrabbah <floflo97231@live.fr>
2023-11-14 20:33:06 +0100

    Ajoutduclone

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,61 @@
 class Admin::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
-
-  def update
-
-    redirect_to root_path
+def new
+    @user = User.new
+end
+    def index
+    @users = User.all 
+  
+  
+    end
+  
+  def edit_user
+    @user = User.find(params[:id])
+    # Logique pour la modification d'utilisateur
   end
+  
+  def update_user
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # Rediriger vers la liste des utilisateurs avec un message de succès
+    else
+      # Rediriger vers le formulaire de modification avec des erreurs
+    end
+  end
+  
+   def destroy_user
+    @user = User.find(params[:id])
+    @user.destroy
+    # Rediriger vers la liste des utilisateurs avec un message de succès
+  
+   end
+  
+   private
+  
+   def authenticate_admin
+     # Vérifier si l'utilisateur est un administrateur
+     # Rediriger vers la page de connexion si ce n'est pas le cas
+   end
+  
+  
+   def user_params
+  
+    # Paramètres autorisés pour la modification de l'utilisateur
+   end
+  
+  
+  
+  
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
 
   
-end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+
+
+    @events = Event.all
   end
+    
 end
+
